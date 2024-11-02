42c3912e2b6715a23501c3b90a457e11d274ffec
GUNNAM MAHESH <gunnamm25@gmail.com>
2024-11-02 16:12:41 +0530

    no change

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // specify the port you want to use
+    host: '0.0.0.0', // allow access from any IP address on your network
+  },
   build: {
-    outDir: 'dist' // ensure this matches your vercel.json
-  }
+    outDir: 'dist', // ensure this matches your vercel.json
+  },
+  publicDir: 'public', 
 });
